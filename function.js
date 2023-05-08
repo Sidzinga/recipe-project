1a6a3e2b3960f31e0252437c541e924fc7bf8c1a
Kgothatso <tshilambwanakgothatso@gmail.com>
2023-05-08 17:22:14 +0200

    recipe project

---
+++ function.js
@@ -0,0 +1,11 @@
+
+const x = a*3
+const y = b*10
+const z = c*25
+
+function s3() { 
+    let a = document.getElementsByClassName("f.1");
+    let b = document.getElementsByClassName("f.2");
+    let c = document.getElementsByClassName("f.3");  
+     
+}
